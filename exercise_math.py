5ee37a7c1651f9a3bd5ba3a611fb4577d596272f
Paulina Alonso <alonsopaulina32@gmail.com>
2025-08-11 13:01:53 -0300

    Add files via upload

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,14 @@
 def math():
     a = 57
     b = 7
+    print(a+b) #suma 
+    resta = a - b 
+    print(resta) #resta 
+    print(a * b) #producto 
+    print((a+b)/2) #promedio
+    producto = a *b  
+    print (a//b) #cociente 
+    print(a%b) # resto del cociente 
+    print(a/b) #valor real de la división
+#math() 
+
